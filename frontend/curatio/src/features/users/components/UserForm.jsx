bbb6b4015ba1f38363389fecc6a47298438b4926
Alejandro <alejandropcj@gmail.com>
2026-02-14 09:37:19 -0500

    feat:campos de tipo label

--- frontend/curatio/src/features/users/components/UserForm.jsx
+++ frontend/curatio/src/features/users/components/UserForm.jsx
@@ -7,11 +7,22 @@ export default function UserForm() {
 
       <form>
         <Input label="Nombre y Apellidos" 
-                placeholder="Juan Rivera Grisales"
+                placeholder="Juan Rivera"
                 
         
         >
+          
         </Input>
+
+         <Input label="Correo electrónico" type="email" placeholder="Ingrese su correo" />
+
+        <Input label="Contraseña" type="password" placeholder="Ingrese su contraseña" />
+
+        <Input label="Fecha de nacimiento" type="date" />
+
+        <Input label="Teléfono" type="tel" placeholder="Ingrese su número de teléfono" />
+
+        <Input label="Edad" type="number" placeholder="Ingrese su edad" />
       </form>
     </div>
   );
