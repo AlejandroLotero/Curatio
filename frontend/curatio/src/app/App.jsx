5047ac88b72e906510adaa84ea1377b1bd42068d
Alejandro <alejandropcj@gmail.com>
2026-02-19 08:26:16 -0500

    Se realiza ajuste en la logica de los botones

--- frontend/curatio/src/app/App.jsx
+++ frontend/curatio/src/app/App.jsx
@@ -1,16 +1,15 @@
-import UserForm from "../features/users/components/UserForm";
+import UserForm from "@/features/users/components/UserForm";
 
 export default function App() {
+  return (
+    <div className="grid grid-cols-1 gap-6  justify-items-center">
+      <h1 className="text-3xl font-bold text-center text-label">
+        Curatio Farma
+      </h1>
 
-    return(
-        <div className="grid grid-cols-1 gap-6  justify-items-center">
-            <h1 className="">
-                Curatio Farma              
-            </h1>
-
-            <div>
-                 <UserForm />
-            </div>
-        </div>
-    )
+      <div>
+        <UserForm />
+      </div>
+    </div>
+  );
 }
--- frontend/curatio/src/shared/components/Input.jsx
+++ frontend/curatio/src/shared/components/Input.jsx
@@ -6,9 +6,12 @@ export default function Input({ label, type = "text", ...props }) {
       {/*label */}
       {label && (
         <label
-          className="block text-[8px]
-                              mb-1
-                              text-gray-600"
+          className="block 
+                            mb-1
+                            text-label
+                            font-body
+                            font-heading
+                            text-mostsmall"
         >
           {label}
         </label>
@@ -17,31 +20,23 @@ export default function Input({ label, type = "text", ...props }) {
       {/*Contenerdor del input */}
 
       <div
-      
-      className="
+        className="
       relative
       h-12
       flex
       items-center
       "
-      
       >
-
-
-
-
         {/* Area interactiva invisible (48px) */}
         <div
           className="
           absolute
           inset-0
           "
-          onMouseDown = {(e) => {
-          e.preventDefault();
-          e.currentTarget.nextSibling.focus();
-        
-        }}
-      
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.currentTarget.nextSibling.focus();
+          }}
         />
 
         {/* Input visual*/}
@@ -50,21 +45,33 @@ export default function Input({ label, type = "text", ...props }) {
           type={type}
           className="
           relative
-           w-full
-           h-10
-           rounded-md
-           border
-           border-black
-           px-4
-           text-base
+          w-full
+          h-10
+          rounded-md
+          border        
+      
+          px-4
+          text-label
+          font-body
+          font-heading
+          text-small
 
-           focus:outline-none
-           focus:ring-1
-
-           focus:ring-green-800
-           focus:border-green-800
           
-           "
+          placeholder:-color-border
+          placeholder:-color-border
+          placeholder:text-small
+          placeholder:font-body
+          placeholder:text-font-heading
+
+
+
+          focus:outline-none
+          focus:ring-1
+
+          focus:ring-color-border
+          focus:border-border
+          
+          "
           {...props}
         />
       </div>
