0d0abc700700c6663c8be1afbe76037af140d9b9
unknown <dayana.grisales@utp.edu.co>
2026-03-02 17:00:45 -0500

    feat: formulario creacion de proveedores

--- frontend/curatio/src/app/router/Index.jsx
+++ frontend/curatio/src/app/router/Index.jsx
@@ -1,8 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate, Outlet} from "react-router-dom";
 import MainLayout from "@/shared/layout/MainLayout";
 import ProfilePage from "@/features/users/pages/ProfilePage";
 import HomePage from "../../features/home/pages/HomePage";
 import AuthLayout from "../../shared/layout/AuthLayout";
+import CreateFormSuppliers from "../../features/suppliers/pages/CreateSuppliersDatosBasicos";
+import DatosContactoSuppliers from "../../features/suppliers/pages/CreateSuppliersDatosContacto";
+
 
 const router = createBrowserRouter([
 
@@ -25,10 +28,23 @@ const router = createBrowserRouter([
             },
 
             {
-                path: "suppliers",
-                element: <h1 className = "p-4"> Proveedor   </h1>
-            },
-
+                    path:"suppliers",
+                    element: <Outlet />,
+                    children: [
+                        {
+                            index: true,
+                            element: <Navigate to="datos-basicos" replace />
+                        },
+                        {
+                            path: "datos-basicos",
+                            element: <CreateFormSuppliers />
+                        },
+                        {
+                            path: "datos-contacto",
+                            element: <DatosContactoSuppliers />
+                        }
+                    ]
+                },
             {
                 path: "products",
                 element: <h1 className = "p-4"> Productos </h1>
---
+++ frontend/curatio/src/features/suppliers/pages/CreateSuppliersDatosBasicos.jsx
@@ -0,0 +1,69 @@
+//Creación de Formulario de Creación de Proveedores - Datos básicos
+import { Link } from "react-router-dom";
+import Input from "../../../shared/components/Input";
+import Button from "../../../shared/components/Button";
+import Select from "@/shared/components/Select";
+// import selectService from "@/features/user/services/serviceSelect";
+// import { getDocumentTypes } from "../users/services/selectService";
+// import { useState, useEffect } from "react";
+
+export default function CreateFormSuppliers() {
+  //   const[documentTypes, setDocumentTypes] = useState([])
+
+  // useEffect (() =>{
+  //     getDocumentTypes().then(setDocumentTypes);
+  // },[]);
+
+  const handleNitChange = (e) => {
+    console.log("NIT del proveedor: ", e.target.value);
+    if (e.target.value === "") {
+      console.log("Este campo no puede estar vacío");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-transparent p-8 rounded-lg w-[400px] space-y-8">
+        {/* DATOS BÁSICOS */}
+        <section>
+          <h2 className="text-xl font-bold text-center mb-6">Datos básicos</h2>
+          <div className="space-y-6 grid grid-cols-1">
+            <Input
+              label="NIT: Ejemplo: 80000000-0"
+              placeholder="80000000-0"
+              onChange={handleNitChange}
+            />
+            <Input
+              label="Nombre del proveedor"
+              placeholder="Juan Rivera"
+            />
+            <Input label="Razón social" placeholder="Mi Empresa S.A.S." />
+            {/* <Select label="Tipos de documento" name="documentType" options={documentTypes}></Select> */}
+          </div>
+        </section>
+
+        {/* Botones de acción */}
+        <div className="flex justify-center gap-4 mt-6">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => console.log("Oprimió cancelar")}
+          >
+            Cancelar
+          </Button>
+
+          <Link to="/contacto/datos-contacto">
+            <Button
+              variant="primary"
+              size="md"
+              type="button"
+              onClick={() => console.log("Ir a datos de contacto")}
+            >
+              Siguiente
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
