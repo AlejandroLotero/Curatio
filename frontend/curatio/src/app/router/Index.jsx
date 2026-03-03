b3f37edb89f275e3a6d8e0b1488dbdd0cd641ee8
unknown <dayana.grisales@utp.edu.co>
2026-03-03 16:09:13 -0500

    feat:Formulario actualizado para crear y registrar proveedores con su respectivo componente

--- frontend/curatio/src/app/router/Index.jsx
+++ frontend/curatio/src/app/router/Index.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter, Navigate, Outlet} from "react-router-dom";
 import MainLayout from "@/shared/layout/MainLayout";
 import ProfilePage from "@/features/users/pages/ProfilePage";
+<<<<<<< HEAD
 import HomePage from "@/features/home/pages/HomePage";
 import AuthLayout from "@/shared/layout/AuthLayout";
 import LoginPage from "@/features/auth/pages/LoginPage";
@@ -9,6 +10,18 @@ import TokenPasswordPage from "@/features/auth/pages/TokenPasswordPage"
 import ResetPasswordPage from "@/features/auth/pages/ResetPasswordPage"
 import DatosBasicosPage from "@/features/users/pages/DatosBasicosPage";
 import DatosContactoPage from "@/features/users/pages/DatosContactoPage";
+=======
+import HomePage from "../../features/home/pages/HomePage";
+import AuthLayout from "../../shared/layout/AuthLayout";
+import CreateFormSuppliers from "../../features/suppliers/pages/CreateSuppliersDatosBasicos";
+import DatosContactoSuppliers from "../../features/suppliers/pages/CreateSuppliersDatosContacto";
+import LoginPage from "@/features/auth/pages/LoginPage";
+import Forgot Password Page from "@/features/auth/pages/Forgot PasswordPage"
+import TokenPassword Page from "@/features/auth/pages/TokenPasswordPage"
+import ResetPassword Page from "@/features/auth/pages/ResetPasswordPage"
+import DatosBasicos Page from "@/features/users/pages/Datos BasicosPage"; 
+import Datos ContactoPage from "@/features/users/pages/Datos ContactoPage";
+>>>>>>> 4b56862 (feat:Formulario actualizado para crear y registrar proveedores con su respectivo componente)
 import RolPage from "@/features/users/pages/RolPage";
 
 const router = createBrowserRouter([
