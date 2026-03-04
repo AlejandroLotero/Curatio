e63f34035260d0c3e7b868b96d6bed2d09d3cc38
unknown <dayana.grisales@utp.edu.co>
2026-03-04 11:31:41 -0500

    fix: aplicación del modal al formulario de creación de proveedores

--- frontend/curatio/src/features/suppliers/pages/CreateSuppliersDatosBasicos.jsx
+++ frontend/curatio/src/features/suppliers/pages/CreateSuppliersDatosBasicos.jsx
@@ -61,6 +61,7 @@ export default function CreateFormSuppliers() {
 
           {/* Botones de acción */}
           <div className="flex justify-between w-full max-w-[320px] mt-6">
+            <Link to="/">
             <Button
               variant="secondary"
               size="sm"
@@ -68,6 +69,7 @@ export default function CreateFormSuppliers() {
             >
               Cancelar
             </Button>
+            </Link>
 
             <Link to="../datos-contacto">
               <Button
