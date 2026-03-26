a34ccba76925cf103bab6f58f010854613ee92f3
Alejandro <alejandropcj@gmail.com>
2026-03-26 01:29:20 -0500

    rutas

--- frontend/curatio/src/features/products/index.js
+++ frontend/curatio/src/features/products/index.js
@@ -1,9 +1,10 @@
 export {default as ProductsPage}  from "./admin/pages/ProductsPage";
 export {default as ProductDetailPage} from "./admin/pages/ProductDetailPage"
 export {default as ListProductsPage} from "./admin/pages/ListProductsPage"
+export {default as EditProductPage} from "./admin/pages/EditProductPage"
+export {default as ProductShowPage} from "./pages/ProductShowPage"
 // export {default as ProductListPage} from "./admin/pages/ProductListPage"
 // export {default as ProductsPage}  from "./pages/ProductsPage";
 
 // export {default as ProductDetailPage} from "./pages/ProductDetailPage";
 // export {default as ListProductsPage} from "./pages/ListProductsPage";
-export {default as ProductShowPage} from "./pages/ProductShowPage"
